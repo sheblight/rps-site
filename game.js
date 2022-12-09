25ca70db6bc9e90c59838e2a51d38616a800d028
sheblight <sheblight@gmail.com>
2022-12-08 21:15:06 -0500

    Transferred game interface to HTML, refactor functionality using JS, add retry feature

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-function getComputerChoice() {
+function getCPUChoice() {
     let value = Math.random();
     if (value > 2/3) {
         return "ROCK";
@@ -11,9 +11,9 @@ function getComputerChoice() {
     }
 }
 
-function determineVictory(playerSelection, computerSelection) {
+function determineVictory(playerSelection, cpuSelection) {
 
-    switch (computerSelection) {
+    switch (cpuSelection) {
         case "ROCK":
             switch (playerSelection) {
                 case "ROCK":
@@ -21,27 +21,27 @@ function determineVictory(playerSelection, computerSelection) {
                 case "PAPER":
                     return "PLAYER";
                 case "SCISSORS":
-                    return "COMPUTER";
+                    return "CPU";
                 default:
                     return "TIE";
             }
         case "PAPER":
             switch (playerSelection) {
                 case "ROCK":
-                    return "COMPUTER";
+                    return "CPU";
                 case "PAPER":
                     return "TIE";
                 case "SCISSORS":
                     return "PLAYER";
                 default:
-                    return "COMPUTER";
+                    return "CPU";
             }
         case "SCISSORS":
             switch (playerSelection) {
                 case "ROCK":
                     return "PLAYER";
                 case "PAPER":
-                    return "COMPUTER";
+                    return "CPU";
                 case "SCISSORS":
                     return "TIE";
                 default:
@@ -52,49 +52,67 @@ function determineVictory(playerSelection, computerSelection) {
     }
 }
 
-function playRound(playerSelection, computerSelection) {
+function playRound(playerSelection, cpuSelection, playerChoiceElement, cpuChoiceElement) {
     let playerChoice = playerSelection.toUpperCase();
+
     // If player choice is undefined, throw rock.
     if (playerChoice != "ROCK" && playerChoice != "PAPER" && playerChoice != "SCISSORS") {
         playerChoice = "ROCK";
     }
-    alert(`Player throws ${playerChoice}, Computer throws ${computerSelection}!`);
-    return determineVictory(playerChoice, computerSelection);
+    playerChoiceElement.textContent = `Player throws ${playerChoice}!`;
+    cpuChoiceElement.textContent = `CPU throws ${cpuSelection}!`;
+    return determineVictory(playerChoice, cpuSelection);
 }
 
+let playerScore = 0;
+let cpuScore = 0;
+const playerScoreText = document.querySelector(".player-score");
+const cpuScoreText = document.querySelector(".cpu-score");
+const playerChoiceText = document.querySelector(".player-choice");
+const cpuChoiceText = document.querySelector(".cpu-choice");
+const postGameElement = document.querySelector("div.post-game");
+const winnerText = document.querySelector(".winner");
+const buttonChoices = document.querySelectorAll('button.choice');
+const retryButton = document.querySelector('button.retry');
 
-function game() {
-    let playerScore = 0;
-    let cpuScore = 0;
-    for (let i=0; i<5;i++) {
-        let choice = prompt(`Round ${i+1}: Rock, paper, scissors... (what is your choice?) `);
-        let result = playRound(choice, getComputerChoice());
-        
-        // Determine round result
-        switch (result) {
-            case "PLAYER":
-                playerScore++;
-                break;
-            case "COMPUTER":
-                cpuScore++;
-                break;
-            default:
-                break;
-        }
-        alert(`Round ${i+1}: Player score ${playerScore}, Computer score ${cpuScore}`);
+function onPlayerMove() {
+    const buttonClass = this.classList[0];
+    const roundWinner = playRound(buttonClass, getCPUChoice(), playerChoiceText, cpuChoiceText);
+    switch (roundWinner) {
+        case "CPU":
+            cpuScore++;
+            break;
+        case "PLAYER":
+            playerScore++;
+            break;
+        default:
+            break;
     }
+    playerScoreText.textContent = `Player: ${playerScore}`;
+    cpuScoreText.textContent = `CPU: ${cpuScore}`;
 
-    // Declare final result
-    if (playerScore > cpuScore) {
-        alert("Player wins!");
-    }
-    else if (playerScore == cpuScore) {
-        alert("The game is a tie!");
-    }
-    else {
-        alert("Computer wins!");
-    }
+    if (playerScore < 5 && cpuScore < 5) return; 
+    // update winner result and show post-game elements
+    const gameWinner = Math.max(playerScore, cpuScore) == playerScore ? "Player" : "CPU";
+    winnerText.textContent = `${gameWinner} wins the game!`;
+    postGameElement.style.display = "block";
 
+    // remove this event from all listeners if victory determined
+    buttonChoices.forEach(button => button.removeEventListener('click', onPlayerMove));
 }
 
-console.log(game());
+function onResetGame() {
+    playerScore = 0;
+    cpuScore = 0;
+    playerScoreText.textContent = `Player: ${playerScore}`;
+    cpuScoreText.textContent = `CPU: ${cpuScore}`;
+    playerChoiceText.textContent = "Make your move...";
+    cpuChoiceText.textContent = "";
+    
+    // hide post game elements
+    postGameElement.style.display = "none";
+    buttonChoices.forEach(button => button.addEventListener('click', onPlayerMove));
+}
+
+onResetGame();
+retryButton.addEventListener('click', onResetGame)
